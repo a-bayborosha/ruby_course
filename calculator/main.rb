1f5ca47948bb10781087404472ca9713fd1a17ba
a-bayborosha <58770693+a-bayborosha@users.noreply.github.com>
2023-01-15 20:22:29 +0100

    Add files via upload
    #with class-check for division by 0

--- calculator/main.rb
+++ calculator/main.rb
@@ -1,10 +1,23 @@
 $stdout.sync = true
 puts "First number:"
-first_num = STDIN.gets.chomp.to_f
+first_num = STDIN.gets.chomp
 puts "Second number:"
-sec_num = STDIN.gets.chomp.to_f
+sec_num = STDIN.gets.chomp
 puts "Choose operation: + - * / "
 operant = STDIN.gets.chomp
+
+
+if first_num.include? "."
+  first_num = first_num.to_f
+else
+  first_num = first_num.to_i
+end
+if sec_num.include? "."
+  sec_num = sec_num.to_f
+else
+  sec_num = sec_num.to_i
+end
+
 
 case operant
   when "+"
@@ -14,10 +27,14 @@ case operant
   when "*"
     puts "Result: #{(first_num * sec_num).round(3)}"
   when "/"
-    begin
-      puts "Result: #{(first_num / sec_num).round(3)}"
-    rescue ZeroDivisionError => error
-      puts "You can't divide by zero!: #{error.message}"
+    if first_num.class == Integer
+      begin
+        puts "Result: #{(first_num / sec_num).round(3)}"
+      rescue ZeroDivisionError => error
+        puts "You can't divide by zero!: #{error.message}"
+      end
+    else
+        puts "Result: #{(first_num / sec_num).round(3)}"
     end
   else
     puts "There's no this operation"
